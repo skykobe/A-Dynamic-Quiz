c8ee135279da5a1dbb90984886087a431abc2705
SKYKOBE <lacushui123@163.com>
2015-08-11 00:13:49 +0800

    use bootstrap.js to update

--- js/M.js
+++ js/M.js
@@ -1,36 +1,52 @@
-				var AllCookie = document.cookie ;
-				var T_P = AllCookie.indexOf('=');
-				if(T_P+1 != AllCookie.length && T_P !=- 1) { //直接登入
-					$("#LogSign").css('display', 'none');
-					$(".frame").fadeIn(1000);
-					var XHR = new XMLHttpRequest();
-			  		XHR.onload = function() {
-				  		ShowQues(XHR);
-			  		}
-			  		XHR.open("get","resource/Question.json",true);
-			  		XHR.send();
-					$("#_word").text('Welcome Back'+' '+'Dear '+document.cookie.substring(T_P+1,AllCookie.length)+': ').fadeIn();
-					$("#CheckOut").fadeIn();
-				}
-				else {
-					
-				$("#log").click(ForLog);
-				$("#sign").click(ForSign);
-				$("#clear").click(ForClean);		
-			  	var XHR = new XMLHttpRequest();
-			  	XHR.onload = function() {
-				  	ShowQues(XHR)
-			  	}
-			  	XHR.open("get","resource/Question.json",true);
-			  	XHR.send();
-				}
-				
-				function SetLocal(name,value) {
-					localStorage.setItem(name,value);
-				}
-				
-				function GetLocal(name) {
-					return localStorage.getItem(name);
-				}
-				
-				//var 
+var AllCookie = document.cookie;
+var T_P = AllCookie.indexOf('=');
+if (T_P + 1 != AllCookie.length && T_P != -1) { //直接登入
+    //$("#LogSign").css('display', 'none');
+    BlockChange("#LogSign", ".frame");
+    //$(".frame").fadeIn(1000);
+    BlockChange('LogSign', 'ChoiceFrame');
+    BlockChange('', '#t_cont');
+    //$("#t_cont").fadeIn(1000);
+    //$("#ChoiceFrame").fadeIn(1000);
+    BlockChange('', '#ChoiceFrame');
+
+    var XHR = new XMLHttpRequest();
+    XHR.onload = function () {
+        ShowQues(XHR);
+    }
+    XHR.open("get", "resource/Question.json", true);
+    XHR.send();
+    $("#_word").text('Welcome Back' + ' ' + 'Dear ' + document.cookie.substring(T_P + 1, AllCookie.length) + ': ').fadeIn();
+    $("#CheckOut").fadeIn();
+}
+else {
+
+    $("#log").click(ForLog);
+    $("#sign").click(ForSign);
+    $("#clear").click(ForClean);
+    var XHR = new XMLHttpRequest();
+    XHR.onload = function () {
+        ShowQues(XHR)
+    }
+    XHR.open("get", "resource/Question.json", true);
+    XHR.send();
+}
+
+function SetLocal(name, value) {
+    localStorage.setItem(name, value);
+}
+
+function GetLocal(name) {
+    return localStorage.getItem(name);
+}
+
+function NewXHR(edit) {
+    var NXHR = new XMLHttpRequest();
+    NXHR.onload = function () {
+        ShowQues(NXHR);
+    };
+    NXHR.open('get', 'resource/Question' + edit + '.json');
+    NXHR.send();
+}
+
+//var
--- js/view.js
+++ js/view.js
@@ -5,14 +5,20 @@ var Qnum ;//= AllQuestion.question.length;
 var hisChoice = [];
 var fullMark ;//= Qnum * 2;
 
+$(".rina").on('mouseenter', function () {
+    $(this).animate({height: '550px'});
+});
 
-				
+$(".rina").on('mouseleave', function () {
+    $(this).animate({height: '450px'});
+});
+
 function ShowQues(XHR) {
 	AllQuestion = JSON.parse(XHR.responseText);
 	Qnum = AllQuestion.question.length;
-	fullMark = Qnum * 2
-	$("#1").text(AllQuestion.question[num]);
-	$("#2").text(AllQuestion.choice[num][0]);
+    fullMark = Qnum * 2;
+    $("#1").text(AllQuestion.question[num]);//
+    $("#2").text(AllQuestion.choice[num][0]);//
 	$("#3").text(AllQuestion.choice[num][1]);
 	$("#4").text(AllQuestion.choice[num][2]);
 	$("#qnum").text('1/'+Qnum);
@@ -23,7 +29,7 @@ function ChangeQues() {
 	$("#2").css('display','none');
 	$("#3").css('display','none');
 	$("#4").css('display','none');
-	if(num == Qnum)
+    if (num === Qnum)
 	{
 	   showPKQ();
 	}
@@ -32,18 +38,18 @@ function ChangeQues() {
 	   $("#2").text(AllQuestion.choice[num][0]).fadeIn(500);
 	   $("#3").text(AllQuestion.choice[num][1]).fadeIn(500);
 	   $("#4").text(AllQuestion.choice[num][2]).fadeIn(500);
-	   if(num == Qnum-1) {
+        if (num === Qnum - 1) {
 			$("#next").val('交卷');
 		  }
 	 }
 }
 
 function BlockChange(now,after) {
-	$('#'+now).css('display','none');
-	$('#'+after).fadeIn(1000);
+    $(now).css('display', 'none');
+    $(after).fadeIn(1000);
 }
 
-function ClearChoice(n) {
+function ReviewChoice(n) { //用于显示上下题，并且回显之前选择的题
 	$("[type='checkbox']").each(function(index, element) {
           $(this).prop('checked',false);
      });
@@ -54,21 +60,34 @@ function ClearChoice(n) {
      });
 }
 
+function CleanChoice() {
+    $("[type='checkbox']").each(function (index, element) {
+        $(this).prop('checked', false);
+    });
+}
+
 function showPKQ() {
 	if(point < 0.5*fullMark) {
 		$(".frame").fadeOut(100);
-		$('img').attr('src','resource/img/1.jpg').slideDown();
-		$("span#word").text('渣渣！！~~~~你的分数是：' + point + '分');
+        $("#ChoiceFrame").fadeOut(100);
+        $('#end').attr('src', 'resource/img/1.jpg').slideDown();
+        $("span#word").html('渣渣！！~~~~你的分数是：' + point + '分' + '<br>点击图片重新测试');
+        $(".rina").fadeOut();
 	}
 	else if(point < fullMark) {
 			$(".frame").fadeOut(100);
-			$('img').attr('src','resource/img/2.jpg').slideDown();
-			$("span#word").text('GOOD JOB！~~~~你的分数是：' + point + '分');
+        $("#ChoiceFrame").fadeOut(100);
+        $('#end').attr('src', 'resource/img/2.jpg').slideDown();
+        $("span#word").html('GOOD JOB！~~~~你的分数是：' + point + '分' + '<br>点击图片重新测试');
+        $(".rina").fadeOut();
 		}
 		else {
 			$(".frame").fadeOut(100);
-			$('img').attr('src','resource/img/3.jpg').slideDown();
-			$("span#word").text('要不要那么厉害，这都满分~~~~你的分数是：' + point + '分');
+        $("#ChoiceFrame").fadeOut(100);
+        $('#end').attr('src', 'resource/img/3.jpg').slideDown();
+        //$("span#word").text('要不要那么厉害，这都满分~~~~你的分数是：' + point + '分');
+        $("span#word").html('要不要那么厉害，这都满分~~~~你的分数是：' + point + '分' + '<br>点击图片重新测试');
+        $(".rina").fadeOut();
 		}
 }
 							
